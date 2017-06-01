d022b1a15b913263e2c23120b9786e79390283a6
Giuliano <Giuliano@Note-PC>
2017-05-31 21:45:37 -0300

    Marcação de botao clicado ou nao

--- projetoBingo/src/projetoBingo/Cartela.java
+++ projetoBingo/src/projetoBingo/Cartela.java
@@ -1,6 +1,7 @@
 package projetoBingo;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -13,6 +14,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
 
 public class Cartela extends JFrame {
@@ -60,92 +62,234 @@ public class Cartela extends JFrame {
 		
 		JButton btnB = new JButton("B");
 		panel_1.add(btnB);
+		btnB.setEnabled(false);
 		
 		JButton btnI = new JButton("I");
 		panel_1.add(btnI);
+		btnI.setEnabled(false);
 		
 		JButton btnN = new JButton("N");
 		panel_1.add(btnN);
+		btnN.setEnabled(false);
 		
 		JButton btnG = new JButton("G");
 		panel_1.add(btnG);
+		btnG.setEnabled(false);
 		
 		JButton btnO = new JButton("O");
 		panel_1.add(btnO);
+		btnO.setEnabled(false);
 		
 		JButton button = new JButton("1");
 		panel_1.add(button);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button);
+			}
+		});
+		
+		
 		
 		JButton button_1 = new JButton("2");
 		panel_1.add(button_1);
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_1);
+			}
+		});
 		
 		JButton button_2 = new JButton("3");
 		panel_1.add(button_2);
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_2);
+			}
+		});
 		
 		JButton button_3 = new JButton("4");
 		panel_1.add(button_3);
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_3);
+			}
+		});
 		
 		JButton button_4 = new JButton("5");
 		panel_1.add(button_4);
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_4);
+			}
+		});
 		
 		JButton button_5 = new JButton("6");
 		panel_1.add(button_5);
+		button_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_5);
+			}
+		});
 		
 		JButton button_6 = new JButton("7");
 		panel_1.add(button_6);
+		button_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_6);
+			}
+		});
 		
 		JButton button_7 = new JButton("8");
 		panel_1.add(button_7);
+		button_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_7);
+			}
+		});
 		
 		JButton button_8 = new JButton("9");
 		panel_1.add(button_8);
+		button_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_8);
+			}
+		});
 		
 		JButton button_9 = new JButton("10");
 		panel_1.add(button_9);
+		button_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_9);
+			}
+		});
 		
 		JButton button_10 = new JButton("11");
 		panel_1.add(button_10);
+		button_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_10);
+			}
+		});
 		
 		JButton button_11 = new JButton("12");
 		panel_1.add(button_11);
+		button_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_11);
+			}
+		});
 		
 		JButton btnBingo = new JButton("BINGO");
 		panel_1.add(btnBingo);
+		btnBingo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(btnBingo);
+			}
+		});
 		
 		JButton button_12 = new JButton("14");
 		panel_1.add(button_12);
+		button_12.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_12);
+			}
+		});
 		
 		JButton button_13 = new JButton("15");
 		panel_1.add(button_13);
+		button_13.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_13);
+			}
+		});
 		
 		JButton button_14 = new JButton("16");
 		panel_1.add(button_14);
+		button_14.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_14);
+			}
+		});
 		
 		JButton button_15 = new JButton("17");
 		panel_1.add(button_15);
+		button_15.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_15);
+			}
+		});
 		
 		JButton button_16 = new JButton("18");
 		panel_1.add(button_16);
+		button_16.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_16);
+			}
+		});
 		
 		JButton button_17 = new JButton("19");
 		panel_1.add(button_17);
+		button_17.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_17);
+			}
+		});
 		
 		JButton button_18 = new JButton("20");
 		panel_1.add(button_18);
+		button_18.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_18);
+			}
+		});
 		
 		JButton button_19 = new JButton("21");
 		panel_1.add(button_19);
+		button_19.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_19);
+			}
+		});
 		
 		JButton button_20 = new JButton("22");
 		panel_1.add(button_20);
+		button_20.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_20);
+			}
+		});
 		
 		JButton button_21 = new JButton("23");
 		panel_1.add(button_21);
+		button_21.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_21);
+			}
+		});
 		
 		JButton button_22 = new JButton("24");
 		panel_1.add(button_22);
+		button_22.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_22);
+			}
+		});
 		
 		JButton button_23 = new JButton("25");
-		panel_1.add(button_23);
+		panel_1.add(button_23);	
+		button_23.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				marcaBotao(button_23);
+			}
+		});
+		
 	}
+	
+	public void marcaBotao(JButton botao){
+		Color marcado= botao.getBackground();
+		if(marcado != Color.GREEN)
+				botao.setBackground(Color.GREEN);
+		else
+			botao.setBackground(null);
+	}
+	
 }
