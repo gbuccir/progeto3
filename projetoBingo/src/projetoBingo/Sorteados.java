9273dca2149e137a1791fea36893abccf73f9ea6
Giuliano <Giuliano@Note-PC>
2017-06-10 21:28:31 -0300

    thread zera a fila

--- projetoBingo/src/projetoBingo/Sorteados.java
+++ projetoBingo/src/projetoBingo/Sorteados.java
@@ -1,37 +1,55 @@
 package projetoBingo;
 
+import javax.swing.JOptionPane;
+
 public class Sorteados implements Runnable {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;	
+	private static final long serialVersionUID = 1L;
 	private Fila f;
 	private Boolean bingo;
 
-	public Sorteados( Fila f, Boolean bingo) {
-		// super();		
+	public Sorteados(Fila f, Boolean bingo) {
+		// super();
 		this.f = f;
 		this.bingo = bingo;
 	}
 
-
 	@Override
 	public void run() {
 		int i = 0;
-		while (true){
+		while (true) {
+			bingo = true;
 
-			
-		while (i <= f.getNumeros().size()-1) {
-			System.out.println(f.getNumeros().get(i).toString() + "fora da thread");
-			i++;
-		}
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			while (i <= f.getNumeros().size() - 1) {
+				System.out.println(f.getNumeros().get(i).toString() + "fora da thread");
+				i++;
+				bingo = false;
+
+			}
+
+			if (bingo == false) {
+				JOptionPane.showMessageDialog(null, "Login invalido");
+				View telaLogin = new View();
+				telaLogin.setVisible(true);
+				//dispose();
+			}
+			else{
+				
+			}
+
+			/*
+			 * if (i > 0){ ganhou = false; System.out.println("ganhou"); }
+			 */
+
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
